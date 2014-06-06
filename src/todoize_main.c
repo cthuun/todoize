af62b4a318a054284d46a0e204b50bc866fdd1cd
Jérôme Kurtz <cthuun@cthuun.fr>
2014-06-06 13:48:57 +0200

    Indenting and printing help

--- src/todoize_main.c
+++ src/todoize_main.c
@@ -4,10 +4,17 @@
 #include <todoize_options.h>
 #include <todoize_error.h>
 
+static inline todoize_help_display(char* name)
+{
+  fprintf(stdout, "%s - %s\n", PACKAGE_NAME, PACKAGE_VERSION);
+  fprintf(stdout, "--\n\n");
+  fprintf(stdout, "--help\t-h\t\tDisplay help (this)\n");
+}
+
 int main(int argc, char** argv)
 {
   t_todoize_options todoize_options;
-  fprintf(stdout, "Welcome to %s - %s\n", PACKAGE_NAME, PACKAGE_VERSION);
+
   if (todoize_getopt(argc, argv, &todoize_options))
   {
     fprintf(stderr, "Fail analyze options - please double check them (try \"%s -h\")\n", argv[0]);
@@ -15,6 +22,7 @@ int main(int argc, char** argv)
   }
   if (todoize_options.display_help)
   {
+    todoize_help_display(argv[0]);
     return TODOIZE_ERROR_HELP;
   }
   return TODOIZE_ERROR_NONE;

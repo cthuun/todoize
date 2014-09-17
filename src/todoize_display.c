10ebdc2e5746b639c8c80529b1bc8e2cb78df0de
Jérôme Kurtz <cthuun@cthuun.fr>
2014-09-17 16:47:28 +0200

    Work on comments

--- src/todoize_display.c
+++ src/todoize_display.c
@@ -23,23 +23,23 @@ static void todoize_display_new_entry()
 static void todoize_display_entries()
 {
   clear();
-  printw("Hello World !!!");  /* Print Hello World      */
-  refresh();      /* Print it on to the real screen */
+  printw("Hello World !!!"); /* Print Hello World */
+  refresh(); /* Print it on to the real screen */
 }
 
 /**
  * \return #e_todoize_error
- * \brief test
+ * \brief This funtion process the user events.
  * This is the main function of the program.
  *
  */
 int todoize_display_main(void)
 {
-  int key_pressed = 0;
-  initscr();      /* Start curses mode      */
+  int key_pressed = 0; /**< The key which is pressed */
+  initscr(); /* Start curses mode */
   noecho(); /* Do not print on screen when getch() */
   todoize_display_entries();
-  while (((key_pressed = getch()) != 'q'))      /* Wait for user input */
+  while (((key_pressed = getch()) != 'q')) /* Wait for user input */
   {
     switch (key_pressed) {
       case 'h':
@@ -54,6 +54,6 @@ int todoize_display_main(void)
     }
     todoize_display_entries();
   }
-  endwin();     /* End curses mode      */
-  return TODOIZE_ERROR_NONE;
+  endwin(); /* End curses mode */
+  return TODOIZE_ERROR_NONE; /**< Everything goes right let's quit */
 }
